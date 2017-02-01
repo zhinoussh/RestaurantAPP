f6f071a1b34d1e31571e5cc45253689da4fff7a3
zhinoussh <spring1986@gmail.com>
2017-02-01 15:11:23 +0330

    add swap fuctionallity to next and previous buttons

--- AndelosAPP/app/src/main/java/com/android/shahkar/andelosapp/activities/MainActivity.java
+++ AndelosAPP/app/src/main/java/com/android/shahkar/andelosapp/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 import com.android.shahkar.andelosapp.R;
 import com.android.shahkar.andelosapp.adapters.CategoryPagerAdapter;
@@ -15,19 +16,22 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private CategoryService categoryServcie;
+    private static int swap_position=0;
+    private ViewPager pager_category;
+    private int categoryCount=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         categoryServcie = new CategoryService();
-
-
         categoryServcie.CategoryList(new CategoryService.ResultCallback() {
             @Override
             public void OnResultReady(List<RestaurantCategory> categoryList) {
                 if (categoryList != null) {
-                    ViewPager pager_category = (ViewPager) findViewById(R.id.pager_category);
+                    categoryCount=categoryList.size();
+                    pager_category = (ViewPager) findViewById(R.id.pager_category);
                     PagerAdapter da = new CategoryPagerAdapter(getSupportFragmentManager(), categoryList);
                     pager_category.setAdapter(da);
                 }
@@ -38,4 +42,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void btnSwapRight_Click(View view) {
+       if(swap_position<categoryCount-1)
+            swap_position++;
+        pager_category.setCurrentItem(swap_position);
+    }
+
+    public void btnSwapLeft_Click(View view) {
+        if(swap_position>0)
+            swap_position--;
+        pager_category.setCurrentItem(swap_position);
+    }
 }
