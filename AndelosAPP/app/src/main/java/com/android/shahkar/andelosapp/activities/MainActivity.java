63665c8dd7e002b8eb8aae313a0d5ce385e05fda
zhinoussh <spring1986@gmail.com>
2017-02-05 10:11:50 +0330

    fix issue with device size

--- AndelosAPP/app/src/main/java/com/android/shahkar/andelosapp/activities/MainActivity.java
+++ AndelosAPP/app/src/main/java/com/android/shahkar/andelosapp/activities/MainActivity.java
@@ -3,9 +3,12 @@ package com.android.shahkar.andelosapp.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
 import com.android.shahkar.andelosapp.R;
 import com.android.shahkar.andelosapp.fragments.ErrorDialogFragment;
 import com.android.shahkar.andelosapp.utils.NetworkConnectivity;
+import com.android.shahkar.andelosapp.utils.ScreenUtility;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +23,15 @@ public class MainActivity extends AppCompatActivity {
         if(!netStatus.checkNetworkStatus())
             showConnectionErrorDialog();
         else {
-            FrameLayout subCategoryContainer = (FrameLayout) findViewById(R.id.SubCategory_fragment_container);
+            FrameLayout subCategoryContainer = (FrameLayout) findViewById(R.id.menu_fragment_container);
             isWideScreen = (subCategoryContainer != null);
+//            ScreenUtility su=new ScreenUtility(this);
+//            if(su.getDpWidth()>=600)
+//                isWideScreen=true;
+//            else
+//                isWideScreen=false;
+//            Toast.makeText(this,"wide: "+su.getDpWidth()+isWideScreen,Toast.LENGTH_LONG).show();
+
         }
 
     }
