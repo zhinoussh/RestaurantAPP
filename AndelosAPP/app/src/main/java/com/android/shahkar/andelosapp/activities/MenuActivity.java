f3212b8671802c957d00a12dfdd67cef826cc208
zhinoussh <spring1986@gmail.com>
2017-02-09 17:57:30 +0330

    add app bar to manu activity

--- AndelosAPP/app/src/main/java/com/android/shahkar/andelosapp/activities/MenuActivity.java
+++ AndelosAPP/app/src/main/java/com/android/shahkar/andelosapp/activities/MenuActivity.java
@@ -1,8 +1,10 @@
 package com.android.shahkar.andelosapp.activities;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.shahkar.andelosapp.R;
 import com.android.shahkar.andelosapp.fragments.MenuListFragment;
@@ -13,6 +15,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Typeface font_AppBar=Typeface.createFromAsset(getAssets(),"fonts/LobsterTwo-Bold.ttf");
+        TextView txt_AppName=(TextView)findViewById(R.id.txt_ApppName);
+        txt_AppName.setTypeface(font_AppBar);
 
         try {
             int CategoryID =getIntent().getIntExtra("CategoryID",0);
